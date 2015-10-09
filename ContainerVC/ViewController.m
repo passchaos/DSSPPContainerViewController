b90a5f94185681233d480f9aad7497e3c79ebba3
Greedwolf DSS <greedwolf.dss@gmail.com>
2015-10-09 14:24:17 +0800

    Initial Commit

---
+++ ContainerVC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ContainerVC
+//
+//  Created by passchaos on 15/10/9.
+//  Copyright © 2015年 passchaos. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
